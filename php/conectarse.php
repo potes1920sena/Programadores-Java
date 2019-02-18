1a45f27126bd4d0ff6127748be88c3c6f85306dc
potes1920sena <38663780+potes1920sena@users.noreply.github.com>
2019-02-17 23:17:21 -0500

    Add files via upload

---
+++ php/conectarse.php
@@ -0,0 +1,11 @@
+<?php
+
+function conectate (&$conexion)
+{
+    $servidor = "localhost";
+    $basededatos = "id3712707_plataformataxi";
+    $usuario = "id3712707_administrador";
+    $clave = "sena1318333";
+    $conexion = mysqli_connect($servidor, $usuario,$clave,$basededatos)or die (mysqli_error($conexion));//Conexion base de datos
+}
+?>
