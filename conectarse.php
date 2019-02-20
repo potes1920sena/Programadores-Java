9e476330d9e6fce69a940f5efffbb38dde34f96b
potes1920sena <38663780+potes1920sena@users.noreply.github.com>
2019-02-20 00:50:37 -0500

    Update conectarse.php

--- conectarse.php
+++ conectarse.php
@@ -2,7 +2,7 @@
 
 function conectate (&$conexion)
 {
-    $servidor = "https://databases-auth.000webhost.com/sql.php?server=1&db=id3712707_plataformataxi&table=conductores&pos=0&token=88d94de1579de06bb85359391a89d76b";
+    $servidor = "localhost";
     $basededatos = "id3712707_plataformataxi";
     $usuario = "id3712707_administrador";
     $clave = "sena1318333";
