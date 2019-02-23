86679269ccd0dab929d3538f5860a6a59458bf09
potes1920sena <38663780+potes1920sena@users.noreply.github.com>
2019-02-22 22:25:41 -0500

    Add files via upload

---
+++ conectarse.php
@@ -0,0 +1,26 @@
+<?php
+
+$conectar=@mysql_connect('localhost','root','');
+
+if(!$conectar){
+	echo"No se pudo Conectar con el Servidor";
+}else{
+	$base=mysql_select_db('paginaweb');
+	if (!$base) {
+		echo"No se encontro la base de datos";
+	}
+}
+//recupera variables
+	$nombre=$_POST['nombre'];
+	$correo=$_POST['correo'];
+	$mensaje=$_POST['mensaje'];
+	$sql="INSERT INTO datos VALUES('$nombre','$correo',$mensaje')";
+	//sentecia sql
+	$ejecutar=mysql_query($sql);
+
+	if(!$ejecutar){
+		echo"Se presento un error";
+	}else{
+		echo"Datos guardados correctamente<br><a href='index.html'>Volver</a>";
+	}
+?>
